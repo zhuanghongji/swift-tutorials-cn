8a11a539ff4c7f9b84e4057cb12591294c416e2e
庄宏基 <zhuanghongji.com@gmail.com>
2023-06-11 16:52:00 +0800

    feat: add Badge component

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+import Badge from "@site/src/components/Badge";
+import ScreenshotBlock from "@site/src/components/ScreenshotBlock";
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  Badge,
+  ScreenshotBlock,
+};
