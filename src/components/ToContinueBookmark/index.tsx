e614a29efa63eca7a7bf709fa530e262bc343c79
庄宏基 <zhuanghongji.com@gmail.com>
2023-06-15 22:40:07 +0800

    docs: checked part of the-basic

---
+++ src/components/ToContinueBookmark/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from './index.module.css'
+
+export default function ToContinueBookmark() {
+  return (
+    <>
+      <br />
+      <div className={styles.container}>待继续的标签</div>
+      <br />
+      </>
+  )
+}
+
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -5,6 +5,7 @@ import DocCaution from "@site/src/components/DocCaution";
 import OriginalLink from "@site/src/components/OriginalLink";
 import ExperimentBlock from "@site/src/components/ExperimentBlock";
 import ScreenshotBlock from "@site/src/components/ScreenshotBlock";
+import ToContinueBookmark from "@site/src/components/ToContinueBookmark";
 
 export default {
   // Re-use the default mapping
@@ -14,4 +15,5 @@ export default {
   OriginalLink,
   ExperimentBlock,
   ScreenshotBlock,
+  ToContinueBookmark,
 };
