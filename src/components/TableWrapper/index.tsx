0d21cac20027cbc47031820126950ebd2a2776ba
庄宏基 <zhuanghongji.com@gmail.com>
2023-08-29 20:18:43 +0800

    style: metal-shading-language format

--- src/components/TableWrapper/index.tsx
+++ src/components/TableWrapper/index.tsx
@@ -1,25 +1,22 @@
 import React, { PropsWithChildren } from 'react'
-import MDXContent from '@theme/MDXContent';
-import CodeBlock from '@theme/CodeBlock';
-import './index.module.css'
 
 type ScreenshotProps = PropsWithChildren<{
   num: string 
   title: string
-  headers: string[]
-  datas: string[][]
 }>
 
 /**
  * 表格容器
  */
-export default function TableWrapper({ num, title, headers, datas, children  }: ScreenshotProps) {
+export default function TableWrapper({ num, title, children  }: ScreenshotProps) {
   
   return (
     <div style={{ display: 'flex', flexDirection: 'column', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
       <center>
         {children}
       </center>
+      <div><strong>{`表 ${num} - ${title}`}</strong></div>
+      <br/>
     </div>
   )
 }
