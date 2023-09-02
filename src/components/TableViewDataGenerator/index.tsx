b0220f08146bfd3793b5fa64fe09a7d268d9b8e9
庄宏基 <zhuanghongji.com@gmail.com>
2023-09-02 17:59:58 +0800

    style: metal-shading-language format

--- src/components/TableViewDataGenerator/index.tsx
+++ src/components/TableViewDataGenerator/index.tsx
@@ -1,66 +1,29 @@
 import React, { useMemo } from 'react'
 
 // 一行的个数
-const INPUT_LINE_NUM = 2
+const INPUT_LINE_NUM = 3
 
 // 在一行中要进行换行，用 \t 代替，解析时会自动替换成 \n
 const INPUT_TEXT = `
-Function 
-Description 
+Convert From 
+To 
+Conversion Rule 
 
-P bezier(Ps_t, P p0, P p1, P p2) 
-Returns the position on a quadratic Bézier curve 
+32-bit signed integer 
+8-bit signed integer 
+result = convert_char_saturate(val) 
 
-P bezier_derivative(Ps_t, P p0, P p1, P p2) 
-Returns the first derivative on a quadratic Bézier curve 
+32-bit signed integer 
+16-bit signed integer 
+result = convert_short_saturate(val) 
 
-P bezier_second_derivative(Ps_t, P p0, P p1, P p2) 
-Returns the second derivative on a quadratic Bézier curve 
+32-bit unsigned integer 
+8-bit unsigned integer 
+result = convert_uchar_saturate(val) 
 
-P bezier(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the position on a cubic Bézier curve 
-
-P bezier_derivative(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the first derivative on a cubic Bézier curve 
-
-P bezier_second_derivative(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the second derivative on a cubic Bézier curve 
-
-P bspline(Ps_t, P p0, P p1, P p2) 
-Returns the position on a quadratic B-spline curve 
-
-P bspline_derivative(Ps_t, P p0, P p1, P p2) 
-Returns the first derivative on a quadratic B-spline curve 
-
-P bspline_second_derivative(Ps_t, P p0, P p1, P p2) 
-Returns the second derivative on a quadratic B-spline curve 
-
-P bspline(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the position on a cubic B-spline curve 
-
-P bspline_derivative(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the first derivative on a cubic B-spline curve 
-
-P bspline_second_derivative(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the second derivative on a cubic B-spline curve 
-
-P hermite(Ps_t, P p0, P p1, P m0, P m1) 
-Returns the position on a cubic Hermite curve 
-
-P hermite_derivative(Ps_t, P p0, P p1, P m0, P m1) 
-Returns the first derivative on a cubic Hermite curve 
-
-P hermite_second_derivative(Ps_t, P p0, P p1, P m0, P m1) 
-Returns the second derivative on a cubic Hermite curve 
-
-P catmull_rom(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the position on a Catmull-Rom curve 
-
-P catmull_rom_derivative(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the first derivative on a Catmull-Rom curve 
-
-P catmull_rom_second_derivative(Ps_t, P p0, P p1, P p2, P p3) 
-Returns the second derivative on a Catmull-Rom curve
+32-bit unsigned integer 
+16-bit unsigned integer 
+result = convert_ushort_saturate(val) 
 `
 
 /**
